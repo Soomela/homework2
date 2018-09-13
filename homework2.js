8d39b66c7ec42756a95012e2913a831171e79070
Student Generic Account <ADStudent@auaad.aua.am>
2018-09-13 17:52:47 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,16 @@
+const x = 1;  //task #1 #3 constant 1 (number)
+const y = 2;  //task #1 #3 constant 2 (number)
+const str = "hello";  //task #1 #3 constant 3 (string)
+const str2 = "world";  //task #1 #3 constant 4 (string)
+
+const z = x + y; //task #2 arithmetic operation
+
+const d = x / y; //task #2 arithmetic operation
+
+const fstr = str + " " + str2;  //task #2 arithmetic operation
+
+if( y > 1) {
+    console.log(fstr);  //task #5 printing something to console (printing line 10)
+} else {
+    console.log("error!");
+}   //task #4 if and else
